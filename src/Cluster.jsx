4afcd0f9001d1a16ce2f722d3e8b03709bc30644
Kelly <kgilliamcode@gmail.com>
2017-10-12 22:17:45 -0700

    add react environ

---
+++ src/Cluster.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Cluster extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        
+      };
+    }
+}
+
+export default Cluster;
