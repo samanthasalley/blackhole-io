b1a9eaf83374bf533cf209ef154b2371d100762d
Kelly <kgilliamcode@gmail.com>
2017-10-14 12:46:33 -0700

    add paper style and background opacity

--- src/InfiniteSpace.jsx
+++ src/InfiniteSpace.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class InfiniteSpace extends React.Component {
   render() {
     return (
-      <div id="canvasWrapper"></div>
+      <MuiThemeProvider>
+        <div id="canvasWrapper"></div>
+      </MuiThemeProvider>
     )
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -3,17 +3,35 @@ import { render } from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Cluster from './Cluster.jsx';
 import InfiniteSpace from './InfiniteSpace.jsx';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {grey300, cyan500} from 'material-ui/styles/colors';
 // import injectTapEventPlugin from 'react-tap-event-plugin';
 
 
+// const muiTheme = getMuiTheme({
+//   palette: {
+//     textColor: cyan500,
+//   },
+//   appBar: {
+//     color: grey300,
+//     textColor: cyan500,
+//     backgroundColor: cyan500,
+//     height: 500
+//   },
+// });
+
 document.addEventListener('DOMContentLoaded', function () {
   render(
+   
+   <MuiThemeProvider muiTheme={getMuiTheme()}> 
     <BrowserRouter>
       <div>
-        <Route path='/' component={Cluster} />
-        <Route path='/InfiniteSpace' component={InfiniteSpace} />
+          <Route path='/' component={Cluster} />
+          <Route path='/InfiniteSpace' component={InfiniteSpace} />
       </div>
-    </BrowserRouter>,
+    </BrowserRouter>
+   </MuiThemeProvider>,
     document.getElementById('root')
   );
 });
