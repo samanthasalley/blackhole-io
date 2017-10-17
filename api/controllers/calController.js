f8f0a10ffa7acd1f85613cd178d7dcaa8e4e1c3a
ChrisKim <ckimchris@Chris-Kims-MacBook-Pro-2.local>
2017-10-16 17:27:01 -0700

    update for server controllers

---
+++ api/controllers/calController.js
@@ -0,0 +1,42 @@
+const pg = require('pg');
+const uri = 'postgres://zyfrobhu:NL5fpVCjHv9oTkrS-D_8Lz5yLK2kD8qY@stampy.db.elephantsql.com:5432/zyfrobhu';
+const calController = {};
+
+// /api/cal output in the form of
+/*
+[
+  {
+      "_id": 1,
+      "month": 10,
+      "day": 1,
+      "events": ""
+  },
+  {
+      "_id": 2,
+      "month": 10,
+      "day": 2,
+      "events": ""
+  },
+  [...]
+]
+*/
+calController.getCalEvents = (req, res, next) => {
+  const results = [];
+  pg.connect(uri, (err, client, done) => {
+    if (err) {
+      done();
+      console.log(err);
+      return res.status(500).json({ success: false, data: err });
+    }
+    const query = client.query('SELECT * FROM "Cal";');
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+}
+
+module.exports = calController;
