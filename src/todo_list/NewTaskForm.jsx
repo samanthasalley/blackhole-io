96a9f57d95bc6bd70ca44368cf9e359c7c0f6cc7
Samantha Salley <samanthasalley@gmail.com>
2017-10-18 10:09:05 -0700

    refactor todo and add new form items

--- src/todo_list/NewTaskForm.jsx
+++ src/todo_list/NewTaskForm.jsx
@@ -2,34 +2,53 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
+import DatePicker from 'material-ui/DatePicker';
 import IconButton from 'material-ui/IconButton';
 import FontIcon from 'material-ui/FontIcon';
 
 const NewTaskForm = (props) => {
-  let taskTypes = props.taskTypes.map(type => <MenuItem value={type} primaryText={type} />);
+  let taskTypes = props.taskTypes.map((type, i) => <MenuItem key={i} value={type} primaryText={type} />);
   return (
-    <div>
-      <TextField
-        name='task'
-        hintText='new task here...'
-        value={props.newTask.name}
-        onChange={props.handleNewTaskInputChange}
-      />
-      <SelectField 
-        id='new-task-type'
-        onChange={props.handleNewTaskSelectChange}
-        value={props.newTask.taskType}
-      >
-        {taskTypes}
-      </SelectField>
-      {}
-      <IconButton
-        type="submit"
-        onClick={props.addTask}
-      >
-        <FontIcon className='fa fa-plus' />
-      </IconButton>
-    </div>
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      props.addTask();
+    }}>
+      <div>
+        <TextField
+          name='name'
+          hintText='new task here...'
+          value={props.newTask.name}
+          onChange={props.handleNewTaskInputChange}
+        />
+      </div>
+      <div>
+        <SelectField
+          name='taskType'
+          hintText='select task type...'
+          onChange={props.handleNewTaskSelectChange}
+          value={props.newTask.taskType}
+        >
+          {taskTypes}
+        </SelectField>
+
+      </div>
+      {props.newTask.taskType === 'Todo' || props.newTask.taskType === 'Reminder' ? <div>
+        <DatePicker
+          name='date'
+          locale='en-US'
+          container='inline'
+          mode='landscape'
+          hintText={`add ${props.newTask.taskType} date...`}
+          value={props.newTask.date}
+          onChange={props.handleNewTaskDateChange}
+        />
+      </div> : null}
+      <div>
+        <IconButton type="submit">
+          <FontIcon className='fa fa-plus' />
+        </IconButton>
+      </div>
+    </form>
   );
 };
 
